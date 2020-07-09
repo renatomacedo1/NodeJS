37cfba836d0e6e91bb7e051db872f1572e308d53
renatomacedo1 <macedo.renato@protonmail.com>
2020-07-09 12:38:54 +0100

    deleted app.js and index.html. Added index.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-http
-  .createServer(function(req, res) {
-    res.end("Olá Mundo! Bem vindo!");
-  })
-  .listen(8081);
-
-console.log("Server running");
---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express"); //Como instalou com NPM e está na pasta moduloes, basta colocar o nome do módulo e não o diretorio completo
+
+const app = express();
+
+app.listen(8081); //Liga o servidor na porta. Tem de ser a última do código
+//localhost:8081
